020f983af591a4e7d3311ead2a8d442b48344a46
John <johnartuz@hotmail.com>
2021-12-01 15:19:23 -0500

    Added to write something on the screen

--- client.js
+++ client.js
@@ -1,3 +1,32 @@
+// const net = require("net");
+
+// // establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",
+//     port: 50541,
+//   });
+
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+
+//   conn.on("connect", () => {
+//     console.log(`Successfully connected to game server`);
+//     // conn.write("Name:Jon");
+//   });
+
+//   //handles incoming data
+//   conn.on("data", function (message) {
+//     console.log("server sent:", message);
+//   });
+
+//   conn.on();
+//   return conn;
+// };
+
+// console.log("Connecting ...");
+// connect();
+
 const net = require("net");
 
 // establishes a connection with the game server
@@ -11,9 +40,14 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log(`You're connected`);
+    console.log(`Sucessfully connected to game server`);
+    conn.write("Name: Jon");
   });
 
+  //handles incoming data from the server
+  conn.on("data", function (message) {
+    console.log("server sent:", message);
+  });
   return conn;
 };
 
