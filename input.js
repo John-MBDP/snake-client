a69a735f7fa64db1e959201ee069f9b53d441ec3
John <johnartuz@hotmail.com>
2021-12-01 17:43:30 -0500

    added functionality for input to accept an object that contains all of the methods to interact with the server

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
-const connect = require("./client");
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,6 +13,14 @@ const setupInput = function () {
 const handleUserInput = (data) => {
   if (data === "\u0003") {
     process.exit();
+  } else if (data === "w") {
+    connection.write("Move: up");
+  } else if (data === "s") {
+    connection.write("Move: down");
+  } else if (data === "d") {
+    connection.write("Move: right");
+  } else if (data === "a") {
+    connection.write("Move: left");
   }
 };
 
