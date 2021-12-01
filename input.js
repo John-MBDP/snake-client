d49f2b2b2ea81fded9f4dd987a90ba67a5c2d9d8
John <johnartuz@hotmail.com>
2021-12-01 17:47:34 -0500

    Added message functionality

--- input.js
+++ input.js
@@ -14,12 +14,16 @@ const handleUserInput = (data) => {
   if (data === "\u0003") {
     process.exit();
   } else if (data === "w") {
+    connection.write("Say: MOVE");
     connection.write("Move: up");
   } else if (data === "s") {
+    connection.write("Say: OUT");
     connection.write("Move: down");
   } else if (data === "d") {
+    connection.write("Say: OF");
     connection.write("Move: right");
   } else if (data === "a") {
+    connection.write("Say: WAY");
     connection.write("Move: left");
   }
 };
